996f75c7c6fd2abca13a0afb0cc9fa0caf3fbc47
negarkiran <negarkiran@gmail.com>
2018-06-14 15:03:13 +0530

    Create Person.java

---
+++ model/Person.java
@@ -0,0 +1,40 @@
+package model;
+
+/**
+ * Created by khegde050916 on 4/17/2018.
+ */
+public class Person {
+
+    private String name;
+
+    private Integer age;
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
