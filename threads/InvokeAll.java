6d3b869cb02422b793e32102d230b9d82a257615
negarkiran <negarkiran@gmail.com>
2018-06-14 15:04:18 +0530

    Create InvokeAll.java

---
+++ threads/InvokeAll.java
@@ -0,0 +1,33 @@
+package threads;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class InvokeAll {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newWorkStealingPool();
+
+        List<Callable<String>> callables = Arrays.asList(
+                ()-> "task1",
+                ()-> "task2",
+                ()-> "task3"
+        );
+
+        try {
+            executorService.invokeAll(callables)
+                    .stream()
+                    .map(future -> {
+                        try {
+                            return future.get();
+                        } catch (Exception e) {
+                            throw new IllegalStateException();
+                        }
+                    }).forEach(System.out::println);
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
